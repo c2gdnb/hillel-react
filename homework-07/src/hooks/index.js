ed93346752335f6ee91ebfdca7200771236d3717
volodymir <kolesnik.volodymir@pdffiller.team>
2022-04-25 14:05:17 +0300

    [homework-07] done

--- homework-07/src/hooks/index.js
+++ homework-07/src/hooks/index.js
@@ -1,18 +1,18 @@
 import { USERS_URI } from "../constants";
 import { ALBUMS_URI } from "../constants";
 import { useEffect, useState } from "react";
-import { getContactsList } from "../services/service";
+import { getData } from "../services/service";
 
 export default function useData() {
   const [users, setUsers] = useState([]);
   const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
-    getContactsList(USERS_URI).then((data) => {
+    getData(USERS_URI).then((data) => {
       setUsers(data);
     });
 
-    getContactsList(ALBUMS_URI).then((data) => {
+    getData(ALBUMS_URI).then((data) => {
       setAlbums(data);
     });
   }, []);
--- homework-07/src/services/service.js
+++ homework-07/src/services/service.js
@@ -1,17 +1,5 @@
 import api from "../api/api";
 
-export function getContactsList(url) {
+export function getData(url) {
   return api.get(url).then((resp) => resp.data);
-}
-
-export function createContact(url, contact) {
-  return api.post(url, contact).then((resp) => resp.data);
-}
-
-export function updateContact(url, contact) {
-  return api.put(url + "/" + contact.id).then((resp) => resp.data);
-}
-
-export function deleteContact(url, id) {
-  return api.delete(`${url}/${id}`).then((resp) => resp.data);
 }
